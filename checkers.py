47a5ccd722f6e3c419259b0e04ead0b7cd00ccfc
RGabriel26 <gabrielrosu112@gmail.com>
2023-12-14 02:22:01 +0200

    implemented leyouts for chechers - implicit version

--- checkers.py
+++ checkers.py
@@ -1,16 +1,10 @@
 from PyQt6.QtWidgets import QPushButton
 
 class Checkers(QPushButton):
-    def __init__(self, team, parent, x, y):
+    def __init__(self, team):
         super().__init__()
         self.setObjectName(f'{team}Checker')
         self.setFixedSize(60,60)
-        self.parent = parent
-        self.x = x
-        self.y = y
 
-        self.clicked.connect(self.test_movePosition)
+        # self.clicked.connect(self.test_movePosition)
 
-    def test_movePosition(self):
-        self.move(self.x, self.y)
-        print(self.parent.size())
